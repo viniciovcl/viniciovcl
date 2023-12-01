7dfdd291077cc60953ec99d78d2d36007a26fd7c
viniciovcl <viniciovcl@gmail.com>
2023-11-30 22:54:01 -0300

    commit theme

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,6 @@
 function updateMode() {
     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches)) {
         document.documentElement.classList.add('dark');
     } else {
         document.documentElement.classList.remove('dark');
